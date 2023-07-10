42dc057fce60058e14a63abfd1c8541c2cac2687
fanoosvpn@protonmail.com <fanoosvpn@protonmail.com>
2023-07-10 14:00:56 -0400

    config template 01

--- install/firewall.sh
+++ install/firewall.sh
@@ -3,8 +3,9 @@ iptables-save >>/etc/sysconfig/iptables.backup
 systemctl disable firewalld
 systemctl stop firewalld
 systemctl enable iptables
+systemctl restart iptables
 iptables -A FORWARD -t filter -i FanoosClient -j ACCEPT
 iptables -A FORWARD -t filter -o FanoosClient -j ACCEPT
 iptables -A POSTROUTING -t nat -o FanoosClient -j MASQUERADE
-iptables-save >>/etc/sysconfig/iptables
+iptables-save >/etc/sysconfig/iptables
 systemctl restart iptables
