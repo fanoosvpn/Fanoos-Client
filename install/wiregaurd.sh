5735155466dd2662842cfa8826d810d594f72c6b
fanoosvpn@protonmail.com <fanoosvpn@protonmail.com>
2023-07-29 07:26:23 -0400

    wriegaurd-ui fifth

--- install/wiregaurd.sh
+++ install/wiregaurd.sh
@@ -2,16 +2,17 @@ yum install epel-release https://www.elrepo.org/elrepo-release-7.el7.elrepo.noar
 yum install yum-plugin-elrepo -y
 yum install kmod-wireguard wireguard-tools -y
 
-cat tools/wg-ui.tar.gz.part-* | tar -C / -xzvf -
+cat tools/wg-ui.tar.gz.part-* | tar -C / -xzf -
 
 cp install/files/wgui.service /etc/systemd/system/wgui.service
 cp install/files/wgui.path /etc/systemd/system/wgui.path
-cp install/files/wgui.wgui /etc/systemd/system/wgui.wgui
+cp install/files/wgui.wgui /etc/systemd/system/wgui.wui.service
 
 systemctl enable wgui.path
 systemctl enable wgui.service
-systemctl enable wgui.wgui
+systemctl enable wgui.wui.service
 systemctl start wg-quick@wg0.service
 systemctl start wgui.path
 systemctl start wgui.service
-systemctl start wgui.wgui
+systemctl start wgui.wui.service
+
