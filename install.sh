08e8307f2ab73a2d44dacbdc00c4393fe2109e3a
fanoosvpn@protonmail.com <fanoosvpn@protonmail.com>
2023-09-07 07:02:02 -0400

    change setting to run client & server at the same time

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 ./install/zsh.sh
 ./install/firewall.sh
 echo 201 Fanoos>> /etc/iproute2/rt_tables
-systemctl enable Fanoos
-systemctl start Fanoos
+systemctl enable Fanoos-Client
+systemctl start Fanoos-Client
